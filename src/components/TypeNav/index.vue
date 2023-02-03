e7ecb3ecf05670e82e9e28f3f114e22fa0a27e85
xmd <2191968433@qq.com>
2023-02-03 10:19:55 +0800

    feat:首页三级目录的跳转

--- src/components/TypeNav/index.vue
+++ src/components/TypeNav/index.vue
@@ -1,3 +1,22 @@
+<!-- 
+总结：
+
+  1。关于三级目录跳转问题
+  实现跳转的方式： routerLink , 编程时导航 ，  事件委托
+
+  区别：
+  - routerLink ：
+    - 把a标签换成编程时导航组件，并结合to属性实现路由跳转。 
+    - 存在问题：会重复生成太多的routerLink组件，性能速度上不怎么好
+  - 编程时导航：
+    - 使用a标签，并且给a标签绑定点击事件click ， 使用this.$router.push({name:'Search',query:{....参数}})
+    - 存在问题 ：同时帮帮太多的点击事件了。
+  - 事件委托 ：
+    - 使用编程时导航，并且结合事件委托，把跳转路由点击事件绑定在它们的 【父元素】身上。
+    - 特点： 只绑定一次事件，点击子元素，都可以触发这个事件。
+    
+    - 事件委托原理 ： 判断元素是否在父元素的管辖范围内。 若在，则触发父元素绑定的事件，若不在则不会触发。
+ -->
 <template>
   <!-- 商品分类导航 -->
   <div class="type-nav">
@@ -5,14 +24,19 @@
       <div class="nav-left">
         <h2 class="all">全部商品分类</h2>
         <div class="sort">
-          <div class="all-sort-list2">
+          <div class="all-sort-list2" @click="toSearch">
             <div
               class="item"
               v-for="c1 in baseCategoryList"
               :key="c1.categoryId"
             >
               <h3>
-                <a>{{ c1.categoryName }}</a>
+                <a
+                  :data-name="c1.categoryName"
+                  :data-id="c1.categoryId"
+                  data-level="1"
+                  >{{ c1.categoryName }}</a
+                >
               </h3>
               <div class="item-list clearfix">
                 <div
@@ -22,11 +46,21 @@
                 >
                   <dl class="fore">
                     <dt>
-                      <a>{{ c2.categoryName }}</a>
+                      <a
+                        :data-name="c2.categoryName"
+                        :data-id="c2.categoryId"
+                        data-level="2"
+                        >{{ c2.categoryName }}</a
+                      >
                     </dt>
                     <dd>
                       <em v-for="c3 in c2.categoryChild" :key="c3.categoryId">
-                        <a href="">{{ c3.categoryName }}</a>
+                        <a
+                          :data-name="c3.categoryName"
+                          :data-id="c3.categoryId"
+                          data-level="3"
+                          >{{ c3.categoryName }}</a
+                        >
                       </em>
                     </dd>
                   </dl>
@@ -67,6 +101,20 @@ export default {
     async getBaseCategoryList() {
       const result = await repgetBaseCategoryList();
       this.baseCategoryList = result.slice(0, 15); // 只要15条记录
+    },
+    // 点击三级目录跳转到搜 索页面
+    toSearch(e) {
+      // 获取自定义属性的值
+      const { id, name, level } = e.target.dataset;
+      // 判断是否点击空白区域
+      if (!id) return;
+      this.$router.push({
+        name: 'Search',
+        query: {
+          categoryName: name,
+          [`category${level}Id`]: id,
+        },
+      });
     },
   },
 };
@@ -122,9 +170,10 @@ export default {
             overflow: hidden;
             padding: 0 20px;
             margin: 0;
-
+            width: 100%;
             a {
               color: #333;
+              display: block;
             }
           }
 
@@ -183,6 +232,12 @@ export default {
           }
 
           &:hover {
+            h3 {
+              background: #e1251b;
+              a {
+                color: #fff;
+              }
+            }
             .item-list {
               display: block;
             }

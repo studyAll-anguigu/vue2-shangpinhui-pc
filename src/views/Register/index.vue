6af24e0fbae27d027a1ad43ff3cdd6808b15edab
xmd <2191968433@qq.com>
2023-02-08 20:47:59 +0800

    feat: 完成注册页面的表单的基本验证

--- src/views/Register/index.vue
+++ src/views/Register/index.vue
@@ -8,35 +8,82 @@
           >我有账号，去 <a href="login.html" target="_blank">登录</a>
         </span>
       </h3>
-      <div class="content">
-        <label>手机号:</label>
-        <input type="text" placeholder="请输入你的手机号" />
-        <span class="error-msg">错误提示信息</span>
-      </div>
-      <div class="content">
-        <label>验证码:</label>
-        <input type="text" placeholder="请输入验证码" />
-        <button class="getcode">获取验证码</button>
-        <span class="error-msg">错误提示信息</span>
-      </div>
-      <div class="content">
-        <label>登录密码:</label>
-        <input type="text" placeholder="请输入你的登录密码" />
-        <span class="error-msg">错误提示信息</span>
-      </div>
-      <div class="content">
-        <label>确认密码:</label>
-        <input type="text" placeholder="请输入确认密码" />
-        <span class="error-msg">错误提示信息</span>
-      </div>
-      <div class="controls">
-        <input name="m1" type="checkbox" />
-        <span>同意协议并注册《尚品汇用户协议》</span>
-        <span class="error-msg">错误提示信息</span>
-      </div>
-      <div class="btn">
-        <button>完成注册</button>
-      </div>
+      <ValidationObserver v-slot="{ handleSubmit }">
+        <form @submit.prevent="handleSubmit(onSubmit)">
+          <ValidationProvider
+            name="phone"
+            rules="phoneRequired|phone"
+            v-slot="{ errors }"
+            tag="div"
+            mode="lazy"
+            class="content"
+          >
+            <label>手机号:</label>
+            <input type="text" placeholder="请输入你的手机号" v-model="phone" />
+            <span class="error-msg">{{ errors[0] }}</span>
+          </ValidationProvider>
+          <ValidationProvider
+            name="code"
+            rules="codeRequired|code"
+            v-slot="{ errors }"
+            tag="div"
+            class="content"
+            mode="lazy"
+          >
+            <label>验证码:</label>
+            <input type="text" placeholder="请输入验证码" v-model="code" />
+            <button class="getcode">获取验证码</button>
+            <span class="error-msg">{{ errors[0] }}</span>
+          </ValidationProvider>
+          <ValidationProvider
+            name="password"
+            rules="passwordRequired|password"
+            v-slot="{ errors }"
+            tag="div"
+            class="content"
+            mode="lazy"
+          >
+            <label>登录密码:</label>
+            <input
+              type="text"
+              placeholder="请输入你的登录密码"
+              v-model="password"
+            />
+            <span class="error-msg">{{ errors[0] }}</span>
+          </ValidationProvider>
+          <ValidationProvider
+            name="repassword"
+            :rules="`repasswordRequired|repassword:${password}`"
+            v-slot="{ errors }"
+            tag="div"
+            class="content"
+            mode="lazy"
+          >
+            <label>确认密码:</label>
+            <input
+              type="text"
+              placeholder="请输入确认密码"
+              v-model="repassword"
+            />
+            <span class="error-msg">{{ errors[0] }}</span>
+          </ValidationProvider>
+          <ValidationProvider
+            class="controls"
+            name="isAgree"
+            rules="isAgree"
+            v-slot="{ errors }"
+            tag="div"
+            mode="lazy"
+          >
+            <input name="m1" type="checkbox" v-model="isAgree" />
+            <span>同意协议并注册《尚品汇用户协议》</span>
+            <span class="error-msg">{{ errors[0] }}</span>
+          </ValidationProvider>
+          <div class="btn">
+            <button>完成注册</button>
+          </div>
+        </form>
+      </ValidationObserver>
     </div>
 
     <!-- 底部 -->
@@ -58,8 +105,95 @@
 </template>
 
 <script>
+import { ValidationObserver, ValidationProvider, extend } from 'vee-validate';
+import { required } from 'vee-validate/dist/rules';
+
+// 校验phone是否有值
+// 写法一： 不要修改默认的错误提示文本
+// extend('email', email);
+
+// 写法2： 修改默认提示的文本值
+extend('phoneRequired', {
+  ...required,
+  message: '请输入手机号',
+});
+// 校验手机号是否符合规范
+extend('phone', {
+  validate: (val) => {
+    const phoneReg =
+      /^1((34[0-8])|(8\d{2})|(([35][0-35-9]|4[579]|66|7[35678]|9[1389])\d{1}))\d{7}$/;
+    return phoneReg.test(val);
+  },
+  message: '请输入正确的手机号',
+});
+
+// 验证码
+extend('codeRequired', {
+  ...required,
+  message: '请输入验证码',
+});
+extend('code', {
+  validate: (val) => {
+    const codeReg = /^[0-9]{6}$/;
+    return codeReg.test(val);
+  },
+  message: '请输入正确的验证码',
+});
+
+// 登录密码
+extend('passwordRequired', {
+  ...required,
+  message: '请输入密码',
+});
+extend('password', {
+  validate: (val) => {
+    const reg = /^[0-9A-Za-z]{6,18}$/;
+    return reg.test(val);
+  },
+  message: '请输入正确的密码',
+});
+
+// 确认密码
+extend('repasswordRequired', {
+  ...required,
+  message: '请输入确认密码',
+});
+extend('repassword', {
+  validate: (val, pw) => {
+    return pw.password === val; // 判断跟密码是否相同
+  },
+  message: '两次输入的密码不一致',
+  params: ['password'], // 生民接受参数
+});
+
+// 勾选协议
+extend('isAgree', {
+  validate: (val) => {
+    return val === true;
+  },
+  message: '请勾选同意协议',
+});
+
 export default {
   name: 'XRegister',
+  components: {
+    ValidationObserver,
+    ValidationProvider,
+  },
+  data() {
+    return {
+      phone: '',
+      code: '',
+      password: '',
+      repassword: '',
+      isAgree: false,
+    };
+  },
+  methods: {
+    onSubmit() {
+      alert('Form has been submitted!');
+    },
+  },
 };
 </script>
 
